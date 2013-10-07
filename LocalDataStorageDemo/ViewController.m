553ccc82e0e5c63f13cc2c5c4aa321264dea61e9
Prateek Gianchandani <pkg@fueled.co>
2013-10-07 17:36:29 +0530

    Initial commit

--- LocalDataStorageDemo/ViewController.m
+++ LocalDataStorageDemo/ViewController.m
@@ -10,6 +10,15 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UITextField *userDefaultsTextField;
+@property (strong, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (strong, nonatomic) IBOutlet UITextField *keychainTextField;
+
+- (IBAction)saveInUserDefaultsTapped:(id)sender;
+- (IBAction)saveInPlistFileTapped:(id)sender;
+- (IBAction)saveInKeychainTapped:(id)sender;
+
 @end
 
 @implementation ViewController
@@ -25,5 +34,23 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)saveInUserDefaultsTapped:(id)sender {
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  [defaults setObject:self.userDefaultsTextField.text forKey:@"DemoValue"];
+  [defaults synchronize];
+}
+
+- (IBAction)saveInPlistFileTapped:(id)sender {
+  NSString *path = [[NSBundle mainBundle] pathForResource:@"userInfo" ofType:@"plist"];
+  NSMutableDictionary* plist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+  [plist setValue:self.usernameTextField.text forKey:@"username"];
+  [plist setValue:self.passwordTextField.text forKey:@"passwprd"];
+  [plist writeToFile:path atomically:YES];
+}
+
+- (IBAction)saveInKeychainTapped:(id)sender {
+}
+
 
 @end
